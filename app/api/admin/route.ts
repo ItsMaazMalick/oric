480d2f9b1acea0bd644c89270449331b5e838db2
ItsMaazMalick <maaz93707@gmail.com>
2024-01-01 20:49:26 +0500

    update admin/route.ts

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -2,6 +2,6 @@ import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const admins = await prisma.admin.findMany();
-  return NextResponse.json({ status: 200, admins });
+  // const admins = await prisma.admin.findMany();
+  return NextResponse.json({ message: "HELLO" });
 }
