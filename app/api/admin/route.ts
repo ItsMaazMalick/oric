1bf295a8178aa250bf76d6e1af34941df1da9039
ItsMaazMalick <maaz93707@gmail.com>
2024-01-01 20:52:52 +0500

    delete admin/route.ts

--- app/api/admin/route.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "@/lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  // const admins = await prisma.admin.findMany();
-  return NextResponse.json({ message: "HELLO" });
-}
